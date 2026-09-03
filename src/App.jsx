9581d74ed25a6ae691a1dcd02154e5f5227f6c5d
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-09-04 02:36:08 +0300

    NavBar Finalllyyyyy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './components/Home/Home'
 import Movies from './components/Movies/Movies'
 import Shows from './components/Shows/Shows'
 import MyList from './components/MyList/MyList'
+import Search from './components/Search/Search'
+import Settings from './components/Settings/Settings'
 import Layout from './components/Layout/Layout'
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
       {index: true, element: <Home />},
       {path: 'movies', element: <Movies />},
       {path: 'series', element: <Shows />},
-      {path: 'lists', element: <MyList />}
+      {path: 'lists', element: <MyList />},
+      {path: 'search', element: <Search />},
+      {path: 'settings', element: <Settings />},
     ] }
   ])
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,38 @@
 import { NavLink } from "react-router-dom";
 import logo from '../../assets/logomark-white.svg';
 import NavbarStyles from './Navbar.module.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
 
-    
+
+    let [pillStyle, setPillStyle] = useState({ left: 0, width: 111 });
+
+    const magicPill = (e) => {
+        const isActive = e.currentTarget.getAttribute('aria-current') === 'page';
+        const parentChildren = Array.from(e.currentTarget.parentNode.children);
+        const currentActiveTab = e.currentTarget.parentNode.querySelector('[aria-current="page"]')
+        
+        let finalWidth = isActive ? e.currentTarget.offsetWidth : e.currentTarget.offsetWidth + 24;
+        let finalLeft = !isActive && parentChildren.indexOf(e.currentTarget) > parentChildren.indexOf(currentActiveTab)? e.currentTarget.offsetLeft-24 : e.currentTarget.offsetLeft
+        if (e.target.id == 'home') {
+            setPillStyle({ left: 6, width: 111 });
+            return
+        }
+        setPillStyle({
+            left: finalLeft,
+            width: finalWidth,
+        })
+    }
 
     return (
         <>
             <div className="fixed top-0 left-0 flex w-full z-50 pointer-events-none px-6 lg:px-12 py-6 justify-between items-center">
                 <div>
                     <div>
-                        <NavLink to={'/'} className={`pointer-events-auto`}>
+                        <NavLink onClick={magicPill} to={'/'} className={`pointer-events-auto`}>
                             <div>
-                                <img className='w-10.5 ' src={logo} alt="logo" />
+                                <img id='home' className='w-10.5 ' src={logo} alt="logo" />
                             </div>
                         </NavLink>
                     </div>
@@ -22,41 +40,41 @@ export default function Navbar() {
 
                 <nav className={`hidden lg:flex relative glass-header desktop-nav items-center rounded-full pointer-events-auto gap-1 ${NavbarStyles.nav}`}>
 
-                    <div className={`rounded-full absolute left-0 opacity-100 bg-white -z-10 ${NavbarStyles.pillIndicator} ${NavbarStyles.pillTransition} ${NavbarStyles.pillTransform}`}>
+                    <div className={`rounded-full absolute left-0 opacity-100 bg-white -z-10 ${NavbarStyles.pillIndicator} ${NavbarStyles.pillTransition}`} style={{ left: pillStyle.left, width: pillStyle.width }}>
 
                     </div>
 
-                    <NavLink className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="/" >
-                        <span className={`text-black hidden`}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-house w-4 h-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
+                    <NavLink onClick={magicPill} className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="/" >
+                        <span className={`text-black ${NavbarStyles.icon}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg ${NavbarStyles.icon}" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-house w-4 h-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
                         </span>
                         Home
                     </NavLink>
 
-                    <NavLink className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="movies" >
-                        <span className={`text-black hidden`}>
+                    <NavLink onClick={magicPill} className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="movies" >
+                        <span className={`text-black ${NavbarStyles.icon}`}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-clapperboard w-4 h-4"><path d="M20.2 6 3 11l-.9-2.4c-.3-1.1.3-2.2 1.3-2.5l13.5-4c1.1-.3 2.2.3 2.5 1.3Z"></path><path d="m6.2 5.3 3.1 3.9"></path><path d="m12.4 3.4 3.1 4"></path><path d="M3 11h18v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z"></path></svg>
                         </span>
                         Movies
                     </NavLink>
 
-                    <NavLink className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="series" >
-                        <span className={`text-black hidden`}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-tv w-4 h-4"><path d="m17 2-5 5-5-5"></path><rect width="20" height="15" x="2" y="7" rx="2"></rect></svg>
+                    <NavLink onClick={magicPill} className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="series" >
+                        <span className={`text-black ${NavbarStyles.icon}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg ${NavbarStyles.icon}" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-tv w-4 h-4"><path d="m17 2-5 5-5-5"></path><rect width="20" height="15" x="2" y="7" rx="2"></rect></svg>
                         </span>
                         Shows
                     </NavLink>
 
-                    <NavLink className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="lists" >
-                        <span className={`text-black hidden`}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-bookmark w-4 h-4"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>                        </span>
+                    <NavLink onClick={magicPill} className={`relative z-10 flex items-center justify-center gap-2 px-6 rounded-full text-sm font-medium whitespace-nowrap text-white/60 ${NavbarStyles.navItem}`} to="lists" >
+                        <span className={`text-black ${NavbarStyles.icon}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg ${NavbarStyles.icon}" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-bookmark w-4 h-4"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>                        </span>
                         My List
                     </NavLink>
 
                     <span className={`w-px h-4.5 m-1.5 ${NavbarStyles.divider}`}></span>
 
-                    <button className={`relative z-10 flex items-center justify-center rounded text-white/60 ${NavbarStyles.searchIndicator}`}>
-                        <NavLink to='search' >
+                    <button onClick={magicPill} className={`relative z-10 flex items-center justify-center rounded text-white/60 ${NavbarStyles.searchIndicator}`}>
+                        <NavLink to='search'>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-icon lucide lucide-search w-5 h-5"><path d="m21 21-4.34-4.34"></path><circle cx="11" cy="11" r="8"></circle></svg>
                             </div>
